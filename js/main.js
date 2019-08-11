be4b75c7def959d1e0b2d60dfdd7491765ebd317
Svistov Nikolay <swistov@gmail.com>
2019-08-11 15:46:04 +0300

    Add JS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function changeBg(img){
+    if (confirm('Точно сменить фон?')) {
+            document.body.style.backgroundImage = 'url(' + img.src + ')';
+    } else {
+        alert('Оставляю как и было')
+    }
+}
