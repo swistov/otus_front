203d3fcb42b545a6e90aa62511dfcbcb0187f210
Svistov Nikolai <swistov@gmail.com>
2019-08-18 00:42:52 +0300

    Configure webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-function changeBg(img){
+import getData from './modules/getData';
+import renderCurces from './modules/renderCurces';
+
+function changeBg(img) {
     if (confirm('Точно сменить фон?')) {
-            document.body.style.backgroundImage = 'url(' + img.src + ')';
+        document.body.style.backgroundImage = 'url(' + img.src + ')';
     } else {
         alert('Оставляю как и было');
     }
 }
+
+function some(){
+    console.log('123');
+}
+
+(async function () {
+    const data = await getData();
+    renderCurces(data);
+}());
+
---
+++ src/modules/renderCurces.js
@@ -0,0 +1,21 @@
+export default function renderCurces(data) {
+    const goodsWrapper = document.querySelector('.courses');
+    data.goods.forEach((good) => {
+        const card = document.createElement('div');
+        card.className = 'course';
+        card.innerHTML = `
+                <div class="card" data-category="${good.category}">
+                    <div class="card-body justify-content-between">
+                        <div class="card-price" style="float:right">${good.price} ₽</div><br>
+                        <h5 class="card-title" style="height:50%">${good.title}</h5>
+                        <div class="teacher" style="margin: 0px 10px 10px 0px">
+                            <h7 class="teacher-info">Teacher: ${good.teacher}</h7>
+                        </div>
+                        <button class="btn btn-primary" style="width:45%">О курсе</button>
+                        <button class="btn btn-primary" style="width:45%; float:right">Купить</button>
+                    </div>
+                </div>
+        `;
+        goodsWrapper.appendChild(card);
+    });
+}
