b740bbc12a44ef8b90d1f2071ded3d5c65e7ed87
Svistov Nikolay <swistov@gmail.com>
2019-09-23 16:23:13 +0300

    New webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
+'use strict';
+
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-module.exports = {
-  entry: {
-    main: './src/index.js',
-    style: './scss/style.scss'
-},
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-      }
-    ]
-  },
-  plugins: [ 
 
-    new ExtractTextPlugin(
-      {filename: 'style.css', disable: false, allChunks: true }
-    ),
-    new MiniCssExtractPlugin({
-      filename: 'style.css',
-    }),
-  ]
+module.exports = {
+    entry: {
+        main: './src/index.js',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: ['css-loader', 'sass-loader']
+            })
+        }]
+    },
+
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
 };
